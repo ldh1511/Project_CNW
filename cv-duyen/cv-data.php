2f8ca489e3a8577c81150dcdb218ec0d19857fe0
nduyen2882 <duyen2882@gmail.com>
2021-01-18 20:52:10 +0700

    cv-d

--- cv-duyen/cv-data.php
+++ cv-duyen/cv-data.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 include(ROOT_PATH."/database/db.php");
 $info=selectOne('infomation',['id'=>'2']);
 $skill=selectCol('*','skill_account, skill',"skill_account.skill_id=skill.skill_id and skill_account.id=2");
@@ -7,4 +7,4 @@ $exp=selectAll('experience',['id'=>'2']);
 $acc=selectOne('account',['id'=>'2']);
 $project=selectAll('projects',['id'=>'2']);
 $socialLinks=selectAll('social_links',['id'=>'2']);
-?>
+?> -->
